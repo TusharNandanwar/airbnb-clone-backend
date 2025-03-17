94b449744586da9553f0ed1cfb4a74471aa38ea0
TusharNandanwar <93318949+TusharNandanwar@users.noreply.github.com>
2025-03-18 00:53:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['https://papaya-lily-f49c07.netlify.app/', 'http://127.0.0.1:5173'],
 }));
 
 // async function uploadToS3(path, originalFilename, mimetype) {
